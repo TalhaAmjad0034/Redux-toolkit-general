3602acc94dffede18f19fdd2a83dd6080a103024
Talha Amjad <talha.amjad@tecaudex.com>
2021-11-03 16:50:41 +0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import { decrement, increment, incrementByAmount } from "./redux/counter";
@@ -6,6 +6,7 @@ import { decrement, increment, incrementByAmount } from "./redux/counter";
 // useSelector is using the store to get the data and we are using the dispatch to dispatch the action
 
 function App() {
+  const [ammount, setAmmount] = useState();
   //   // Name of the variable that we want to use in our component and we are getting from the store
   const { count } = useSelector((state) => state.counter); // we are getting the count from the state
 
@@ -15,8 +16,14 @@ function App() {
       <h1> The count is : {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(33))}>
-        Increment by payload
+      <input
+        type="number"
+        onChange={(e) => {
+          setAmmount(parseInt(e.target.value));
+        }}
+      />
+      <button onClick={() => dispatch(incrementByAmount(ammount))}>
+        add this value
       </button>
     </div>
   );
